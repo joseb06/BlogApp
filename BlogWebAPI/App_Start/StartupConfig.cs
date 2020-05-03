8679654d8cd342b5051431ca8167ad45ecb0e1d2
Jose Buitrago <nova_masterj@hotmail.com>
2020-05-03 19:28:57 -0400

    Startup and users Auth functions were refactored

---
+++ BlogWebAPI/App_Start/StartupConfig.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Web.Http;
+using Microsoft.Owin;
+using Microsoft.Owin.Security.OAuth;
+using Owin;
+using BlogWebAPI;
+
+namespace BlogWebAPI
+{
+    public partial class Startup
+    {
+        public void ConfigureAuth(IAppBuilder app)
+        {
+            // Enable CORS (cross origin resource sharing) for making request using browser from different domains
+            app.UseCors(Microsoft.Owin.Cors.CorsOptions.AllowAll);
+
+            OAuthAuthorizationServerOptions options = new OAuthAuthorizationServerOptions
+            {
+                AllowInsecureHttp = true,
+                TokenEndpointPath = new PathString("/Token"),
+                AccessTokenExpireTimeSpan = TimeSpan.FromHours(1),
+                Provider = new AppOauthProvider()
+            };
+            app.UseOAuthAuthorizationServer(options);
+            app.UseOAuthBearerAuthentication(new OAuthBearerAuthenticationOptions());
+
+            HttpConfiguration config = new HttpConfiguration();
+            WebApiConfig.Register(config);
+        }
+    }
+}
--- BlogWebAPI/Startup.cs
+++ BlogWebAPI/Startup.cs
@@ -1,33 +1,16 @@
-﻿using System;
-using System.Threading.Tasks;
-using System.Web.Http;
+﻿using BlogWebAPI;
 using Microsoft.Owin;
-using Microsoft.Owin.Security.OAuth;
 using Owin;
 
-[assembly: OwinStartup(typeof(BlogWebAPI.Startup))]
+[assembly: OwinStartup(typeof(Startup))]
 
 namespace BlogWebAPI
 {
-    public class Startup
+    public partial class Startup
     {
         public void Configuration(IAppBuilder app)
         {
-            // Enable CORS (cross origin resource sharing) for making request using browser from different domains
-            app.UseCors(Microsoft.Owin.Cors.CorsOptions.AllowAll);
-
-            OAuthAuthorizationServerOptions options = new OAuthAuthorizationServerOptions
-            {
-                AllowInsecureHttp = true,
-                TokenEndpointPath = new PathString("/BlogApp/Login"),
-                AccessTokenExpireTimeSpan = TimeSpan.FromHours(1),
-                Provider = new AppOauthProvider()
-            };
-            app.UseOAuthAuthorizationServer(options);
-            app.UseOAuthBearerAuthentication(new OAuthBearerAuthenticationOptions());
-
-            HttpConfiguration config = new HttpConfiguration();
-            WebApiConfig.Register(config);
+            ConfigureAuth(app);
         }
     }
 }
